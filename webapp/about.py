905aa2f386716d052859913dbd6cc018b6a89e1c
Yashaswi <tantri.yashaswi@gmail.com>
2021-07-25 20:34:21 -0500

    Initial Commit-2

---
+++ webapp/about.py
@@ -0,0 +1,10 @@
+import justpy as jp
+
+
+class Home:
+    path = "/"
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+        jp.Div(a=div)
